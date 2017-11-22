cde4b1b186b92d7df40f38f09246a9dbc7de5eb8
Deigo San Cristobal <7582061+DiegoSCB@users.noreply.github.com>
2017-11-22 17:42:48 +0100

    cambio de ruta2

--- move_files.sh
+++ move_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp fix_encoding.sh ~/workspace/data/.
-cp pull_data.sh ~/workspace/data/.
+cp fix_encoding.sh /workspace/data/.
+cp pull_data.sh /workspace/data/.
 
-cp preprocess.sh ~/workspace/OpenNMT-py/.
-cp train.sh ~/workspace/OpenNMT-py/.
-cp infer.sh ~/workspace/OpenNMT-py/.
+cp preprocess.sh /workspace/OpenNMT-py/.
+cp train.sh /workspace/OpenNMT-py/.
+cp infer.sh /workspace/OpenNMT-py/.
