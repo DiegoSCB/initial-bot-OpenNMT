610e4aa9d25d5a78b17c4be6be29f8a0fe045861
Deigo San Cristobal <7582061+DiegoSCB@users.noreply.github.com>
2017-11-23 17:38:15 +0100

    cambio utf_dev.enc a utf_dev.dec

--- preprocess.sh
+++ preprocess.sh
@@ -2,5 +2,5 @@ python preprocess.py \
 	-train_src ../data/utf_train.enc \
 	-train_tgt ../data/utf_train.dec \
 	-valid_src ../data/utf_dev.enc \
-	-valid_tgt ../data/utf_dev.enc \
+	-valid_tgt ../data/utf_dev.dec \
 	-save_data ../data/bot
